e8fd411b008bf8ee4f674cd65d357d371d39b9e9
milimika77 <dianazakh2006@gmail.com>
2026-06-16 13:57:09 +0500

    pr09: add license check CI workflows and SPDX headers

---
+++ pr09/demo-project/app.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024 Мили Мика
+# SPDX-License-Identifier: MIT
+                              
+import requests
+print("demo app")
+
---
+++ pr09/report.md
@@ -0,0 +1,5 @@
+<!--
+SPDX-FileCopyrightText: 2024 Мили Мика <dianazakh2006@gmail.com>
+
+SPDX-License-Identifier: MIT
+-->
